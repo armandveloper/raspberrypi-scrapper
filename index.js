0c29c156e3085d40b13d98a1c3df6f701fb17b2d
Armando Cruz <jaimes087.uz@gmail.com>
2022-08-05 13:04:40 -0500

    refactor: check if has stock in the product page instead of the collections list

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ import { chromium } from 'playwright'
 (async () => {
   const browser = await chromium.launch()
   const page = await browser.newPage()
-  await page.goto('https://www.330ohms.com/collections/raspberry-pi-400')
-  const rapsberryCard = await page.locator('.productitem--info:has-text("Raspberry Pi 400 - Kit en Español")')
-  const stock = await rapsberryCard.locator('text=Stock')
-  console.log(await stock.innerText())
-  const stockCountRaw = await stock.locator('b').innerText()
-  const stockCount = Number(stockCountRaw)
+  await page.goto('https://www.330ohms.com/collections/raspberry-pi-400/products/raspberry-pi-400-kit-oficial-en-espanol')
+  const productDetails = await page.locator('[aria-live="polite"]:has-text("Stock")')
+  const stock = await productDetails.locator('b').innerText()
+  const stockCount = Number(stock)
   const hasStock = stockCount > 0
-  console.log(stockCount)
+  console.log({ stockCount, hasStock })
+  await page.close()
+  await browser.close()
   if (!hasStock) return
   console.log('We have stock!')
   // TODO Send me a notification
-  await browser.close()
 })()
